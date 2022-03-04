a80ee07d236640c2c072a5448d1e2bee9de912c3
hishamshirbini <58462858+hishamshirbini@users.noreply.github.com>
2022-03-04 13:29:44 +0200

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -120,7 +120,7 @@ class HomeController extends Controller
         return view('about');
     }
     function catalogue(){
-        dd(setting('site.pdf')[0]);
+        dd(setting('site.pdf'));
         return  asset('storage/'.setting('site.pdf'));
     }
     function contact(){
