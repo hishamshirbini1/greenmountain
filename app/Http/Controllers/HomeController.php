d42ba123d245bf39d8e8e1d10ff9488877236c64
hishamshirbini <58462858+hishamshirbini@users.noreply.github.com>
2022-03-04 14:25:46 +0200

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -124,7 +124,7 @@ class HomeController extends Controller
         $str = setting('site.pdf');
         // dd(str_replace(['[',']','{','}'], '', $str));
         $array = explode(',', str_replace(['[',']','{','}'], '', $str));
-        dd($array);
+        dd(explode(':',$array[0]));
         dd(asset('storage/'.setting('site.pdf')));
         // return view('pdf',compact('pdf'));
 
