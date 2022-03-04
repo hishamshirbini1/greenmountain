fd8b529af19ee59ef865b0592bb9827b5715b7cb
hishamshirbini <58462858+hishamshirbini@users.noreply.github.com>
2022-03-04 12:16:28 +0200

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -85,10 +85,10 @@
 				</div>
 			</div>
 			<div class="home_product_right">
-				<div class="product_img_title">
+				{{-- <div class="product_img_title">
 					<p>HYGIENE SOLUTION</p>
 					<span>Our products offer you the latest in innovation <br/> and they help you save both money and energy.</span>
-				</div>
+				</div> --}}
 				<img src="assets/images/home_product_img.jpg">
 				<div class="share_icon"></div>
 			</div>
